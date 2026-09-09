4a6a8fbd5a398301ce578b96975b6f610f6ef46a
superChokkk <vcestaemchok@gmail.com>
2026-09-09 02:16:35 -0300

    vercel

--- catalogo-nest-tailwind/src/main.ts
+++ catalogo-nest-tailwind/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   //CORS
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://catalogo-front-vert.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
