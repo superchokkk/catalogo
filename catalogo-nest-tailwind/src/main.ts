1da4305048d33fd4948a7fbf38c6dfbd388686f6
superChokkk <vcestaemchok@gmail.com>
2026-09-09 02:46:49 -0300

    uma barra

--- catalogo-nest-tailwind/src/main.ts
+++ catalogo-nest-tailwind/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   //CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://dgconcept-catalogo.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://dgconcept-catalogo.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
